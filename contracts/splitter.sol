3f5b0138370d2c5c7c78e0b43e4a8e6da6ca80c3
kittykatsky <kd.work65@gmail.com>
2020-08-20 22:41:44 +0100

    [feat] KD - 20.08.20 - F2  creating initial functionality for splitter contract
     Features Added:
     - contract can now review money from the owner
     - owner can now assign recievers (beneficaries of the split) to the
     contract
     - renounce/transfer ownership functions overriden to stop ownership
     changes
    
     TODO:
     - add splitter functionality
     - add withdraw money functionality
    
     Notes:
     following test are now passing:
     - Should have the deployer as its owner
     - Should not have any ETH on deployment
     - Should not have any recievers on deployment
     - Should only be possible for the owner to add recievers
    
     On branch tests
     Changes to be committed:
    	modified:   contracts/splitter.sol

--- contracts/splitter.sol
+++ contracts/splitter.sol
@@ -1,8 +1,41 @@
-pragma solidity ^0.5.4;
+pragma solidity ^0.6.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract splitter {
-    constructor() public {
-                    
+contract splitter is Ownable{
+
+    using SafeMath for uint;
+
+    mapping(address => bool) public receiver;
+    mapping(address => uint) public receiverFunds;
+
+    uint8 public receiverCount;
+
+    event addedReciever();
+    event MoneySent();
+    event MoneyRecieved();
+    event split();
+
+    function addReciever(address _who) public onlyOwner {
+        receiver[_who] = true;
+        receiverCount += 1;
+        emit addedReciever();
     }
+
+    receive() external payable {
+        require(msg.sender == owner(), "Only owner is allowed to deposit");        
+        emit MoneySent();
+    }
+
+    function renounceOwnership() public onlyOwner override {
+         revert("not possible to renounce ownership");
+          
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner override {
+         revert("not possible to change ownership");
+          
+    }
+
 }
