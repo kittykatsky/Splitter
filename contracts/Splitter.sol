045db8ab587a9d40a0f18afdab9225f25738abf2
kittykatsky <kd.work65@gmail.com>
2020-08-23 19:56:28 +0100

    Example:  [doc] KD - 23.08.20 - D1  added various docs/comments
     Features Added:
     - added project description to README
     - added comments to splitter

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.6.0;
 import "./Owned.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
+/// @author Kat
+/// @title A contract for splitting ethereum 
 contract Splitter is Owned{
 
     using SafeMath for uint;
@@ -13,6 +15,10 @@ contract Splitter is Owned{
     event EtherWithdrawnEvent(address indexed sender, uint amount);
     event SplitDoneEvent(address indexed sender, address indexed payee1, address indexed payee2, uint amount);
 
+    /// Split funds between payees
+    /// @param payee1 - first payee
+    /// @param payee2 - second payee
+    /// @dev currently no way of ensuring that the same payees are specified when called multiple times 
     function performSplit(address payable payee1, address payable payee2) public payable onlyOwner {
         require(msg.value > 0, "No ether sent for split");
         require(payee1 != payee2, "only one payee specified");
@@ -20,6 +26,13 @@ contract Splitter is Owned{
         require(payee1 != address(0x0) && payee2 != address(0x0), "incorrect payee specified");
 
         uint payout = msg.value.add(leftOver);
+       
+        /* should the leftOver be returned to the owner?
+        /  currently theres nothing stopping the owner from
+        /  calling the splitter function with different accounts
+        /  possibly giving extra ether to some other account.
+        */
+
         leftOver = payout.mod(2);
         payout = payout.div(2);
 
@@ -29,7 +42,16 @@ contract Splitter is Owned{
         emit SplitDoneEvent(msg.sender, payee1, payee2, payout);
     }
 
+    /// Withdraw an amount of ether
+    /// @param amount the amount of ehter to withdraw
+    /// @dev allows payee to withdraw their alloted funds
+	/// @return true if succesfull
     function withdrawEther(uint amount) public returns (bool) {
+        /* 
+        /
+        /
+        */
+
         require(payeeBalance[msg.sender] > 0, "No Ether in splitter");
         require(amount <= payeeBalance[msg.sender], "More Ether requested than available");
         payeeBalance[msg.sender] -= amount;
@@ -38,7 +60,9 @@ contract Splitter is Owned{
         return true;
     }
 
-    receive () external payable {
+
+    /// Fallback function 
+    /// @dev ensures that no ether can be sent directly to contract
         revert("cant send ether directly");
     }
 }
