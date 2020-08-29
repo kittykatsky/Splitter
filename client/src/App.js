6ccabcd0c5f9287e5fb4f2ad503fb330744fac29
kittykatsky <kd.work65@gmail.com>
2020-08-29 18:45:12 +0100

    [feat] KD - 20.08.20 - W1  Create Splitter instance, web page and interactions
     Features Added:
     - Splitter instanciated
     - Basic HTML added to show all necessary info
     - Split functionality added
     - function to handle interactions added
    
     TODO:
     - There are no addressees:
         Unhandled Rejection (Error): This contract object doesn't have address
         set yet, please set an address first.
     - Cant handle events
        - Stops updates based on events
    
     On branch webapp
     Changes to be committed:
    	modified:   client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
-import splitterContract from "./contracts/splitter.json";
+import splitterContract from "./contracts/Splitter.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { loaded:false, splitterAmout:0, splitAmount:0, ownerAmount:0, payeeOneAddress: "0x0", payeeTwoAddress: "0x0", payeeOneAmount:0, payeeTwoAmount:0 };
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      this.web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      this.accounts = await this.web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = splitterContract.networks[networkId];
-      const instance = new web3.eth.Contract(
+      this.networkId = await this.web3.eth.net.getId();
+
+      this.Splitter = new this.web3.eth.Contract(
         splitterContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        splitterContract.networks[this.networkId] && splitterContract.networks[this.networkId].address,
       );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      //this.listenToTransfer();
+      //this.listenToWithdraw();
+      //this.web3.accounts.on('update', (selectedAddress) => console.log(selectedAddress));
+      this.setState({loaded:true}, this.updateAmounts);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,23 +36,59 @@ class App extends Component {
     }
   };
 
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+        [name]: value
+      });
+  };
+
+  handleBuyTokens = async() => {
+    await this.tokenSaleInstance.methods.performSplit(
+        this.state.payeeOneAddress, this.state.payeeTwoAddress
+    ).send(
+        {from: this.accounts[0], value: this.web3.utils.toWei(this.state.splitAmount, "wei")}
+    );
+  };
+
+  listenToTransfer = () => {
+    //this.Splitter.events.LogSplitDoneEvent().on("data", this.updateAmounts);
+  };
+
+  listenToWithdraw = () => {
+    //this.Splitter.events.LogEtherWithdrawnEvent().on("data", this.updateAmounts);
+  };
+
+  updateAmounts = async () => {
+    const ownerShare = this.Splitter.methods.payeeBalance(this.accounts[0]).call();
+    const payeeOneShare = this.Splitter.methods.payeeBalance(this.state.payeeOneAddress).call();
+    const payeeTwoShare = this.Splitter.methods.payeeBalance(this.state.payeeTwoAddress).call();
+    this.setState({ownerAmount: ownerShare});
+    this.setState({payeeOneAmount: payeeOneShare});
+    this.setState({payeeTwoAmount: payeeTwoShare});
+  }
+
   render() {
-    if (!this.state.web3) {
+    if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Splitter App</h1>
+        <p>Splitter Ballance: {this.state.splitterAmout} Wei</p>
+        <h2>Split Wei</h2>
+        <div>
+            <p>Amount to split: <input type="text" name="splitAmount" value={this.state.splitAmount} onChange={this.handleInputChange} /></p>
+            <p>Payee 1 address: <input type="text" name="payeeOneAddress" value={this.state.payeeOneAddress} onChange={this.handleInputChange} /></p>
+            <p>Payee 2 address: <input type="text" name="payeeTwoAddress" value={this.state.payeeTwoAddress} onChange={this.handleInputChange} /></p>
+            <button type="button" onClick={this.handleSplit}>Split!</button>
+        </div>
+        <div> funds available</div>
+            <p>Owner: {this.state.ownerAmount} Wei</p>
+            <p>Payee 1: {this.state.payeeOneAmount} Wei</p>
+            <p>Payee 2: {this.state.payeeTwoAmount} Wei</p>
       </div>
     );
   }
